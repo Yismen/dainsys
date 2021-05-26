a84976375167558723b52448bf1c875da96e816e
Yismen Jorge <yismen.jorge@gmail.com>
2021-05-26 08:28:20 -0400

    Update deploy script

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -7,35 +7,43 @@
     $serverLink = $root . 'current';
     $branch = 'master';
 @endsetup
+@story('deploy')
+    update-locally
+    copy-project-to-release-folder
+@endstory
 
-@task('deploy', ['on' => 'web'])    
-    
-    cd {{ $releaseFolder }}
-    {{-- ln -sfn {{ $projectFolder }} {{ $serverLink }} --}}
+@task('update-locally', ['on' => 'local'])
+    git push
+@endtask
 
+@task('copy-project-to-release-folder', ['on' => 'web'])  
     [ -d {{ $releaseFolder }} ] || mkdir {{ $releaseFolder }}
+    cd {{ $releaseFolder }}
     {{-- cp -rvfp {{ $projectFolder . "/*" }} {{ $releaseFolder }} --}}
     cp -rvfp {{ $projectFolder }}/[^node_modules][^vendor]* {{ $releaseFolder }}
     cp -rvfp {{ $projectFolder . "/.env" }} {{ $releaseFolder }}
+    chown -R :www-data {{ $releaseFolder }}
     chmod -R 775 {{ $releaseFolder.'/storage' }}
     chmod -R 775 {{ $releaseFolder.'/bootstrap/cache' }}
-    composer install --no-dev -o -n
-
-    php artisan optimize
-    npm install && npm run production
     
     ln -sfn {{ $releaseFolder }} {{ $serverLink }}
-    
+@endtask
+
+@task('deploy-to-production-folder', ['on' => 'web'])
+    {{-- [ -d {{ $projectFolder }} ] || mkdir {{ $projectFolder }} --}}
     cd {{ $projectFolder }}
     git reset --hard
     git pull origin {{ $branch }} --force
     git checkout {{ $branch }}
+
+    ln -sfn {{ $projectFolder }} {{ $serverLink }}
+@endtask
+
+@after
     composer install --no-dev -o -n
     php artisan migrate --force
     npm install && npm run production
     php artisan dainsys:laravel-logs laravel- --clear --keep=8
     php artisan cache:clear
     php artisan optimize
-
-    ln -sfn {{ $projectFolder }} {{ $serverLink }}
-@endtask
+@endafter
