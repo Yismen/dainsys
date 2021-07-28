dd234d5a0265599ee4c0b817dad23acdd84f7bc4
Yismen Jorge <yismen.jorge@gmail.com>
2021-07-27 22:53:50 -0400

    Fix Ooma Schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -66,7 +66,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('inbound:send-daily-summary')->dailyAt('06:20')->timezone('America/New_York');
         $schedule->command('inbound:send-wtd-summary')->dailyAt('06:30')->timezone('America/New_York');
 
-        $schedule->command('ooma:send-daily-production-report')->dailyAt('20:00')->timezone('America/New_York');
+        $schedule->command('ooma:send-production-report')->dailyAt('20:00')->timezone('America/New_York');
 
         $schedule->command('backup:run')->dailyAt('21:15')->timezone('America/New_York');
         $schedule->command('backup:clean')->dailyAt('22:15')->timezone('America/New_York');
