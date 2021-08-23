856872ba7349985661e68f2fa0be8594484678d0
Yismen Jorge <yismen.jorge@gmail.com>
2021-08-23 15:33:14 -0400

    Added more tables to be ignored when dumping

--- config/database.php
+++ config/database.php
@@ -56,6 +56,9 @@ return [
                     'telescope_entries',
                     'telescope_entries_tags',
                     'telescope_monitoring',
+                    'failed_jobs',
+                    'jobs',
+                    'notifications',
                 ],
                 'doNotCreateTables' => true
             ]
