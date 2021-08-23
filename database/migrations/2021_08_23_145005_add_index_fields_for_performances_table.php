fc98bbdd419f2c8146cd40dcc5a46f4786d279dc
Yismen Jorge <yismen.jorge@gmail.com>
2021-08-23 15:05:01 -0400

    Add index fields to performances table
    Added the necessary index fields to improve performance on
    performances table

---
+++ database/migrations/2021_08_23_145005_add_index_fields_for_performances_table.php
@@ -0,0 +1,74 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIndexFieldsForPerformancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // performances
+        Schema::table('performances', function (Blueprint $table) {
+            $table->index('name');
+            $table->index('date');
+        });
+        // supervisors
+        Schema::table('supervisors', function (Blueprint $table) {
+            $table->index('name');
+        });
+        // projects
+        Schema::table('projects', function (Blueprint $table) {
+            $table->index('name');
+        });
+        // campaigns
+        Schema::table('campaigns', function (Blueprint $table) {
+            $table->index('name');
+        });
+        // employees
+        Schema::table('employees', function (Blueprint $table) {
+            $table->index('first_name');
+            $table->index('second_first_name');
+            $table->index('last_name');
+            $table->index('second_last_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // performances
+        Schema::table('performances', function (Blueprint $table) {
+            $table->dropIndex(['name']);
+            $table->dropIndex(['date']);
+        });
+        // supervisors
+        Schema::table('supervisors', function (Blueprint $table) {
+            $table->dropIndex(['name']);
+        });
+        // projects
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropIndex(['name']);
+        });
+        // campaigns
+        Schema::table('campaigns', function (Blueprint $table) {
+            $table->dropIndex(['name']);
+        });
+        // employees
+        Schema::table('employees', function (Blueprint $table) {
+            $table->dropIndex(['first_name']);
+            $table->dropIndex(['second_first_name']);
+            $table->dropIndex(['last_name']);
+            $table->dropIndex(['second_last_name']);
+        });
+    }
+}
