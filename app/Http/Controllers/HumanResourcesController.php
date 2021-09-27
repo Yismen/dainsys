e0c7a3b49612d7127b99587c459727b35b6cc7b9
Yismen Jorge <yismen.jorge@gmail.com>
2021-09-27 16:24:59 -0400

    Add spanish translations

--- app/Http/Controllers/HumanResourcesController.php
+++ app/Http/Controllers/HumanResourcesController.php
@@ -11,20 +11,20 @@ class HumanResourcesController extends Controller
     {
         $employees = BirthdaysRepository::thisMonth()->get();
 
-        return view('human_resources.birthdays.list_monthly', compact('employees'))->with(['title' => 'Birthdays This Month!']);
+        return view('human_resources.birthdays.list_monthly', compact('employees'))->with(['title' => __('Birthdays') . " " . __('This') . " " . __('Month')]);
     }
 
     public function birthdaysNextMonth()
     {
         $employees = BirthdaysRepository::nextMonth()->get();
 
-        return view('human_resources.birthdays.list_monthly', compact('employees'))->with(['title' => 'Birthdays Next Month!']);
+        return view('human_resources.birthdays.list_monthly', compact('employees'))->with(['title' => __('Birthdays') . " " . __('Next') . " " . __('Month')]);
     }
 
     public function birthdaysLastMonth()
     {
         $employees = BirthdaysRepository::lastMonth()->get();
 
-        return view('human_resources.birthdays.list_monthly', compact('employees'))->with(['title' => 'Birthdays Last Month!']);
+        return view('human_resources.birthdays.list_monthly', compact('employees'))->with(['title' => __('Birthdays') . " " . __('Last') . " " . __('Month')]);
     }
 }
