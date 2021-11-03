efe3ca6cbc125aa9d4e38c8b782a3a7e819af5ed
Yismen Jorge <yismen.jorge@gmail.com>
2021-11-03 07:30:00 -0400

    Make object optional

--- resources/views/app/partials/latest-members.blade.php
+++ resources/views/app/partials/latest-members.blade.php
@@ -1,4 +1,4 @@
-<div class="row" >
+<div class="row">
     <div class="col-sm-12">
         <div class="box box-danger">
             <div class="box-header with-border">
@@ -7,21 +7,18 @@
 
             <div class="box-body" style="display: flex; flex-wrap: wrap; justify-content: flex-start;">
                 @foreach ($profiles as $profile)
-                    <div class="col-sm-4">
-                        <img
-                            src="{{ file_exists($profile->photo) ? asset($profile->photo) :  'http://placehold.it/300x300'}}"
-                            class="profile-user-img img-responsive img-circle" alt="Image"
-                        >
+                <div class="col-sm-4">
+                    <img src="{{ file_exists($profile->photo) ? asset($profile->photo) :  'http://placehold.it/300x300'}}" class="profile-user-img img-responsive img-circle" alt="Image">
 
-                        <h5 class="profile-username text-center">
-                            <a href="{{ route('admin.profiles.show', $profile->id) }}" title="Visit {{ $profile->user->name }} Profile">
-                                {{ mb_strimwidth(optional($profile->user)->name, 0, 15, '...') }}
-                            </a>
-                        </h5>
+                    <h5 class="profile-username text-center">
+                        <a href="{{ route('admin.profiles.show', $profile->id) }}" title="Visit {{ optional($profile->user)->name }} Profile">
+                            {{ mb_strimwidth(optional($profile->user)->name, 0, 15, '...') }}
+                        </a>
+                    </h5>
 
-                        <p class="text-center help-block" style="font-size: 10px;">{{ strtoupper($profile->created_at->diffForHumans()) }}</p>
+                    <p class="text-center help-block" style="font-size: 10px;">{{ strtoupper($profile->created_at->diffForHumans()) }}</p>
 
-                    </div>
+                </div>
                 @endforeach
             </div>
         </div>
