263e95c0f223fcf8dd85004474732495cb768e15
Yismen Jorge <yismen.jorge@gmail.com>
2021-11-12 10:42:51 -0400

    Add tests for Downtime Reasons crud

---
+++ tests/Feature/DowntimeReasons/AuthorizationTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature\DowntimeReasons;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthorizationTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    public function testUnuthorizedUsersCantViewDowntimeReason()
+    {
+        $downtime_reason = create('App\DowntimeReason');
+        $response = $this->actingAs($this->userWithPermission('wrong-permission'));
+
+        $response->get(route('admin.downtime_reasons.index'))
+            ->assertForbidden();
+
+        $response->get(route('admin.downtime_reasons.show', $downtime_reason->id))
+            ->assertForbidden();
+    }
+
+    public function testUnuthorizedUsersCantCreatetDowntimeReason()
+    {
+        $downtime_reason = create('App\DowntimeReason');
+        $response = $this->actingAs($this->userWithPermission('wrong-permission'));
+
+        $response->post(route('admin.downtime_reasons.store'))
+            ->assertForbidden();
+    }
+
+    public function testUnuthorizedUsersCantEditDowntimeReason()
+    {
+        $downtime_reason = create('App\DowntimeReason');
+        $response = $this->actingAs($this->userWithPermission('wrong-permission'));
+
+        $response->put(route('admin.downtime_reasons.update', $downtime_reason->id))
+            ->assertForbidden();
+    }
+
+    public function testUnuthorizedUsersCantDestroyDowntimeReason()
+    {
+        $downtime_reason = create('App\DowntimeReason');
+        $response = $this->actingAs($this->userWithPermission('wrong-permission'));
+
+        $response->delete(route('admin.downtime_reasons.destroy', $downtime_reason->id))
+            ->assertForbidden();
+    }
+}
---
+++ tests/Feature/DowntimeReasons/FormValidationTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\DowntimeReasons;
+
+use App\DowntimeReason;
+use App\Employee;
+use App\User;
+use Carbon\Carbon;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class FormValidationTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+
+    /** @test */
+    public function name_field_is_required()
+    {
+        $downtime_reason = create(DowntimeReason::class)->toArray();
+
+        $this->actingAs($this->userWithPermission('create-downtime_reasons'))
+            ->post(route('admin.downtime_reasons.store'), array_merge($downtime_reason, ['name' => '']))
+            ->assertSessionHasErrors('name');
+
+        $this->actingAs($this->userWithPermission('edit-downtime_reasons'))
+            ->put(route('admin.downtime_reasons.update', $downtime_reason['id']), array_merge($downtime_reason, ['name' => '']))
+            ->assertSessionHasErrors('name');
+    }
+}
