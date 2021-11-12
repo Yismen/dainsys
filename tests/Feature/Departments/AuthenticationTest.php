45eb8301d2e91e4105089b9d38c27d7c695620e7
Yismen Jorge <yismen.jorge@gmail.com>
2021-11-12 10:33:24 -0400

    Add tests for Departments crud

---
+++ tests/Feature/Departments/AuthenticationTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature\Departments;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthenticationTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    public function testGuestCantViewDepartments()
+    {
+        $department = create('App\Department');
+
+        $this->get(route('admin.departments.index'))
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+
+        $this->get(route('admin.departments.show', $department->id))
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+    }
+
+    public function testGuestCantCreateDepartments()
+    {
+        $department = create('App\Department');
+
+        $this->get(route('admin.departments.create'))
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+
+        $this->post(route('admin.departments.store'), $department->toArray())
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+    }
+
+    public function testGuestCantUpdateDepartment()
+    {
+        $department = create('App\Department');
+
+        $this->get(route('admin.departments.edit', $department->id))
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+
+        $this->put(route('admin.departments.update', $department->id), $department->toArray())
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+    }
+
+    public function testGuestCantDestroyDepartment()
+    {
+        $department = create('App\Department');
+
+        $this->delete(route('admin.departments.destroy', $department->id))
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+    }
+}
---
+++ tests/Feature/Departments/ModuleActionsTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Feature\Departments;
+
+use App\Department;
+use App\DepartmentCode;
+use App\Employee;
+use App\Supervisor;
+use App\User;
+use Carbon\Carbon;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ModuleActionsTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    /** @test */
+    public function it_lists_all_users_assigned_with_departments()
+    {
+        $user = $this->userWithPermission('view-departments');
+        $this->be($user);
+        $department = create(Department::class);
+
+        $this->get(route('admin.departments.index'))
+            ->assertOk()
+            ->assertViewIs('departments.index');
+    }
+
+    /** @test */
+    public function authorized_users_can_store_department()
+    {
+        $department = make(Department::class)->toArray();
+
+        $this->actingAs($this->userWithPermission('create-departments'))
+            ->post(route('admin.departments.store', $department))
+            ->assertRedirect()
+            ->assertLocation(route('admin.departments.index'));
+
+        $this->assertDatabaseHas('departments', $department);
+    }
+
+    /** @test */
+    public function authorized_users_can_see_edit_page()
+    {
+        $department = create(Department::class);
+
+        $this->actingAs($this->userWithPermission('edit-departments'))
+            ->get(route('admin.departments.edit', $department->id))
+            ->assertOk()
+            ->assertViewIs('departments.edit');
+    }
+
+    /** @test */
+    public function authorized_users_can_update_department()
+    {
+        $department = create(Department::class);
+
+        $data_array = [
+            'name' => 'New Name',
+        ];
+
+        $this->actingAs($this->userWithPermission('edit-departments'))
+            ->put(route('admin.departments.update', $department->id), array_merge($department->toArray(), $data_array))
+            ->assertLocation(route('admin.departments.edit', $department->id));
+
+        $this->assertDatabaseHas('departments', $data_array);
+    }
+
+    /** @test */
+    public function authorized_users_can_destroy_department()
+    {
+        $department = create(Department::class);
+
+        $this->actingAs($this->userWithPermission('destroy-departments'))
+            ->delete(route('admin.departments.destroy', $department->id))
+            ->assertRedirect()
+            ->assertLocation(route('admin.departments.index'));
+
+        $this->assertDatabaseMissing('departments', $department->toArray());
+    }
+}
