5fb7afad0e3872ea427caf05060960f3684f888f
Yismen Jorge <yismen.jorge@gmail.com>
2021-11-17 08:56:48 -0400

    Add tests for roles crud

---
+++ tests/Feature/Roles/AuthenticationTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature\Roles;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthenticationTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    public function testGuestCantViewRoles()
+    {
+        $role = create('App\Role');
+
+        $this->get(route('admin.roles.index'))
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+
+        $this->get(route('admin.roles.show', $role->id))
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+    }
+
+    public function testGuestCantCreateRoles()
+    {
+        $role = create('App\Role');
+
+        $this->get(route('admin.roles.create'))
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+
+        $this->post(route('admin.roles.store'), $role->toArray())
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+    }
+
+    public function testGuestCantUpdateRole()
+    {
+        $role = create('App\Role');
+
+        $this->get(route('admin.roles.edit', $role->id))
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+
+        $this->put(route('admin.roles.update', $role->id), $role->toArray())
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+    }
+
+    public function testGuestCantDestroyRole()
+    {
+        $role = create('App\Role');
+
+        $this->delete(route('admin.roles.destroy', $role->id))
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+    }
+}
---
+++ tests/Feature/Roles/AuthorizationTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature\Roles;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthorizationTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    public function testUnuthorizedUsersCantViewRole()
+    {
+        $role = create('App\Role');
+        $response = $this->actingAs($this->userWithRole('wrong-role'));
+
+        $response->get(route('admin.roles.index'))
+            ->assertForbidden();
+
+        $response->get(route('admin.roles.show', $role->name))
+            ->assertForbidden();
+    }
+
+    public function testUnuthorizedUsersCantCreatetRole()
+    {
+        $role = create('App\Role');
+        $response = $this->actingAs($this->userWithRole('wrong-role'));
+
+        $response->post(route('admin.roles.store'))
+            ->assertForbidden();
+    }
+
+    public function testUnuthorizedUsersCantEditRole()
+    {
+        $role = create('App\Role');
+        $response = $this->actingAs($this->userWithRole('wrong-role'));
+
+        $response->put(route('admin.roles.update', $role->name))
+            ->assertForbidden();
+    }
+
+    public function testUnuthorizedUsersCantDestroyRole()
+    {
+        $role = create('App\Role');
+        $response = $this->actingAs($this->userWithRole('wrong-role'));
+
+        $response->delete(route('admin.roles.destroy', $role->name))
+            ->assertForbidden();
+    }
+}
