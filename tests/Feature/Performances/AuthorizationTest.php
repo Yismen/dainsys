42e97d8c3d4c30a526b3f5b381d8d431d80b147e
Yismen Jorge <yismen.jorge@gmail.com>
2021-11-18 11:45:12 -0400

    Add missing tests for performances module

---
+++ tests/Feature/Performances/AuthorizationTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature\Performances;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthorizationTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    public function testUnuthorizedUsersCantViewPerformance()
+    {
+        $performance = create('App\Performance');
+        $response = $this->actingAs($this->userWithPermission('wrong-permission'));
+
+        $response->get(route('admin.performances.index'))
+            ->assertForbidden();
+
+        $response->get(route('admin.performances.show', $performance->id))
+            ->assertForbidden();
+    }
+
+    public function testUnuthorizedUsersCantEditPerformance()
+    {
+        $performance = create('App\Performance');
+        $response = $this->actingAs($this->userWithPermission('wrong-permission'));
+
+        $response->put(route('admin.performances.update', $performance->id))
+            ->assertForbidden();
+    }
+
+    public function testUnuthorizedUsersCantDestroyPerformance()
+    {
+        $performance = create('App\Performance');
+        $response = $this->actingAs($this->userWithPermission('wrong-permission'));
+
+        $response->delete(route('admin.performances.destroy', $performance->id))
+            ->assertForbidden();
+    }
+}
