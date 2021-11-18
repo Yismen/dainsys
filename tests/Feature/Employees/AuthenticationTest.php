7e408df0b807a4d78cd0e0aef226cea33e216918
Yismen Jorge <yismen.jorge@gmail.com>
2021-11-18 09:28:22 -0400

    Add tests for employees crud

---
+++ tests/Feature/Employees/AuthenticationTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature\Employees;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthenticationTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    public function testGuestCantViewEmployees()
+    {
+        $employee = create('App\Employee');
+
+        $this->get(route('admin.employees.index'))
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+
+        $this->get(route('admin.employees.show', $employee->id))
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+    }
+
+    public function testGuestCantCreateEmployees()
+    {
+        $employee = create('App\Employee');
+
+        $this->get(route('admin.employees.create'))
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+
+        $this->post(route('admin.employees.store'), $employee->toArray())
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+    }
+
+    public function testGuestCantUpdateEmployee()
+    {
+        $employee = create('App\Employee');
+
+        $this->get(route('admin.employees.edit', $employee->id))
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+
+        $this->put(route('admin.employees.update', $employee->id), $employee->toArray())
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+    }
+
+    public function testGuestCantDestroyEmployee()
+    {
+        $employee = create('App\Employee');
+
+        $this->delete(route('admin.employees.destroy', $employee->id))
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
+    }
+}
