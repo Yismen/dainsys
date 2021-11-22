0ab6fc52b408267cab4f84282d07d5f4e7272858
Yismen Jorge <yismen.jorge@gmail.com>
2021-11-22 15:55:42 -0400

    add tests for employee punches

---
+++ tests/Feature/Employee/PunchTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Feature\Employee;
+
+use App\Punch;
+use App\Employee;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Arr;
+use Tests\TestCase;
+
+class PunchTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function employee_punch_is_created()
+    {
+        $employee = create(Employee::class);
+        $punch = make(Punch::class)->toArray();
+
+        $response = $this
+            ->actingAs($this->user())
+            ->post(route('admin.employees.update-punch', $employee->id), $punch);
+
+        $response->assertOk();
+        $this->assertDatabaseHas('punches', array_merge($punch, ['employee_id' => $employee->id]));
+    }
+
+    /** @test */
+    public function employee_punch_is_updated()
+    {
+        $punch = create(Punch::class);
+        $updated_attributes = [
+            'punch' => '99999999',
+        ];
+
+        $response = $this
+            ->actingAs($this->user())
+            ->post(route('admin.employees.update-punch', $punch->employee->id), $updated_attributes);
+
+        $response->assertOk();
+        $this->assertDatabaseHas('punches', $updated_attributes);
+    }
+
+    /** @test */
+    public function employee_punch_data_is_validated()
+    {
+        $punch = create(Punch::class);
+        $updated_attributes = [
+            'punch' => '',
+        ];
+
+        $response = $this
+            ->actingAs($this->user())
+            ->post(route('admin.employees.update-punch', $punch->employee->id), $updated_attributes);
+
+        $response->assertInvalid(['punch']);
+    }
+}
