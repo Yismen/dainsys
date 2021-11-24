62e3b7d01300240f9832026b2a5c8d80ab7e8db7
Yismen Jorge <yismen.jorge@gmail.com>
2021-11-24 10:36:15 -0400

    Add test for api nationality

---
+++ tests/Feature/Api/NationalityTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use App\Nationality;
+use App\Vip;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Passport\Passport;
+use Tests\TestCase;
+
+class NationalityTest extends TestCase
+{
+    use RefreshDatabase;
+
+
+    /** @test */
+    public function a_nationality_can_be_created()
+    {
+        $nationality = make(Nationality::class)->toArray();
+        Passport::actingAs($this->userWithPermission('create-nationalities'));
+
+        $response = $this->post('/api/nationalities', $nationality);
+
+        $response->assertRedirect();
+
+        $this->assertDatabaseHas('nationalities', $nationality);
+    }
+}
