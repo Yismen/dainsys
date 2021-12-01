42277537a8812c0f0487597db4f372673ce34838
Yismen Jorge <yismen.jorge@gmail.com>
2021-12-01 07:47:19 -0400

    Add tests for user settings

--- routes/web.php
+++ routes/web.php
@@ -274,8 +274,8 @@ Route::group(['as' => 'admin.', 'prefix' => 'admin', 'middleware' => 'auth'], fu
     Route::get('users/force_reset/{user}', 'User\PasswordController@force_reset')->name('users.force_reset');
     Route::post('users/force_reset/{user}', 'User\PasswordController@force_change')->name('users.force_change');
 
-    Route::post('users/update_settings/{user}', 'User\SettingController@updateSettings')->name('users.update_settings');
-    Route::put('users/settings/{user}', 'User\SettingController@update')->name('users.settings');
+    // Route::post('users/update_settings/{user}', 'User\SettingController@updateSettings')->name('users.update_settings');
+    Route::post('users/settings', 'User\SettingController@update')->name('users.settings');
 
     Route::get('users/inactive-users', 'UsersController@inactives')->name('users.inactive-users');
     Route::post('users/inactive-users/{id}/restore', 'UsersController@restore')->name('users.inactive-users.restore');
