0a8e8bee65156d2bf64237ff410ad5a4eea7199f
Yismen Jorge <yismen.jorge@gmail.com>
2022-01-10 15:57:51 -0400

    WIP

--- Envoy_new.blade.php
+++ Envoy_new.blade.php
@@ -1,35 +1,47 @@
-@servers(['local' => ['127.0.0.1'], 'web' => ['root@192.168.10.24'], 'web2' => ['yjorge@192.168.10.24']])
+@servers(['production' => 'yjorge@192.168.10.24'])
 
 @setup
-    $root = '/var/www/html/';
-    $releaseFolder = $root .'releases/';
-    $serverLink = $root . 'current';
+    $repo = 'https://github.com/Yismen/dainsys.git';
+    $appDir = '/var/www/dainsys-temp';
     $branch = 'master';
-    $date = date('Y_m_d_H_i_s');
-    $newRelease = $releaseFolder . $date;
+
+    date_default_timezone_set('America/Santo_Domingo');
+    $date = date('YmdHis');
+
+    $builds = $appDir . '/sources';
+    $deployment = $builds . '/' . $date;
+
+    $serve = $appDir . '/htm/current';
+    $env = $appDir . '/.env';
+    $storage = $appDir . '/storage';
 @endsetup
 
-@task('deploy', ['on' => 'web2'])
-    {{-- Create release folder and access it --}}
-    [ -d {{ $releaseFolder }} ] || mkdir {{ $releaseFolder }}
-    [ -d {{ $newRelease }} ] || mkdir {{ $newRelease }}
-    cd {{ $newRelease }}
-    {{-- Clone git repo --}}
-    git clone https://github.com/Yismen/dainsys.git .
-    git checkout {{ $branch }}
-    {{-- Run installation routine --}}
-    cp -r {{ $serverLink . "/.env" }} {{ $newRelease }}
-    composer install --no-dev -o -n
-    chown -R :www-data {{ $newRelease }}
-    chmod -R 775 {{ $newRelease.'/storage' }}
-    chmod -R 775 {{ $newRelease.'/bootstrap/cache' }}
-    php artisan migrate --force
-    php artisan dainsys:laravel-logs laravel- --clear --keep=8
-    php artisan cache:clear
-    php artisan optimize
-    php artisan storage:link
-    cd {{ $newRelease }} && npm install && npm run production
-    {{-- Point the link to the new folder --}}
-    ln -sfn {{ $newRelease }} {{ $serverLink }}
-    rm -Rd {{ $releaseFolder }}!'({{ $date }})'
+@story('deploy')
+    git
+    install
+    live
+@endstory
+
+@task('git', ['on' => 'production'])
+    git clone -b {{ $branch }} "{{ $repo }}" {{ $deployment }}
+@endtask
+
+@task('install', ['on' => 'production'])
+    cd {{ $deployment }}
+    
+    rm -rf {{ $deployment }}/storage
+    
+    ln -nfs {{ $env }} {{ $deployment }}/.env
+    
+    ln -nfs {{ $storage }} {{ $deployment }}/storage
+    
+    composer install --prefer-dist --no-dev
+    
+    php ./artisan migrate --force
+@endtask
+
+@task('live', ['on' => 'production'])
+    cd {{ $deployment }}
+    
+    ln -nfs {{ $deployment }} {{ $serve }}
 @endtask
