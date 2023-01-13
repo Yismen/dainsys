9f8c040ac287091f66f29caf69af998da3874b70
Yismen Jorge <yismen.jorge@gmail.com>
2023-01-13 07:50:37 -0400

    Update frontend

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,15 +1,16 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
 import loading from './modules/loading';
 import employee from './modules/employee';
 
-Vue.use(Vuex)
-
-const debug = process.env.NODE_ENV !== 'production'
-
-export default new Vuex.Store({
+const store = new Vuex.Store({
     modules: {
-        loading, employee
+        loading,
+        employee
     },
-    strict: debug
-})
+    strict: process.env.NODE_ENV !== 'production'
+});
+
+export default store;
