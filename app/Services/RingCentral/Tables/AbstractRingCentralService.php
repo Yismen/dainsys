b2ee377c85c01b47d5a1a895733ababa46ffe3b0
Yismen Jorge <yismen.jorge@gmail.com>
2023-01-30 16:21:47 -0400

    feat: :sparkles: Add ring central manager module

--- app/Services/RingCentral/Tables/AbstractRingCentralService.php
+++ app/Services/RingCentral/Tables/AbstractRingCentralService.php
@@ -2,108 +2,55 @@
 
 namespace App\Services\RingCentral\Tables;
 
-use Exception;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 
-/**
- * @method mixed addField()    Push new item to the default fields array
- * @method mixed datesBetwee() Update the query dates range
- * @method mixed team()        limit to certain team pattern. Default is '%'. Example 'ecc%'
- * @method mixed dialGroup()   limit to certain Dial Group pattern. Default is '%'. Example 'pol%'
- */
 abstract class AbstractRingCentralService
 {
     protected array $fields;
-    protected Carbon $from;
-    protected Carbon $to;
-    protected string $team = '%';
-    protected string $dial_group = '%';
-
-    protected array $default_fields;
+    protected Carbon $date_from;
+    protected Carbon $date_to;
+    protected Builder $query;
 
     public function __construct()
     {
-        $this->default_fields = $this->defaultFields();
-
-        $this->from = now();
-        $this->to = now();
+        $this->date_from = now();
+        $this->date_to = now();
+        $this->query = $this->baseQuery();
     }
 
-    abstract protected function aggregates(): string;
+    abstract protected function baseQuery(): Builder;
 
-    abstract protected function defaultFields(): array;
-
-    abstract public function make(array $fields): Builder;
-
-    public function addField(string $key): self
+    public function datesBetween(string $date_from, string $to = null): self
     {
-        $this->default_fields[] = $key;
+        $this->date_from = Carbon::parse($date_from);
+        $this->date_to = $to ? Carbon::parse($to) : $this->date_from;
 
         return $this;
     }
 
-    public function datesBetween(string $from, string $to = null): self
+    public function groupByDate(): self
     {
-        $this->from = Carbon::parse($from);
-        $this->to = $to ? Carbon::parse($to) : $this->from;
+        $this->query
+            ->groupBy('date')
+            ->orderBy('date');
 
         return $this;
     }
 
-    public function team(string $team = null): self
+    public function build(array $fields = [])
     {
-        $this->team = $team ? $team : '%';
+        $this->query->whereDate('date', '>=', $this->date_from)
+            ->whereDate('date', '<=', $this->date_to)
+            ->addSelect('date');
 
-        return $this;
-    }
-
-    public function dialGroup(string $dial_group = null): self
-    {
-        $this->dial_group = $dial_group ? $dial_group : '%';
-
-        return $this;
-    }
-
-    protected function build(array $fields = [], $model)
-    {
-        $this->protectAgainstInvalidField($fields)
-        ->updateFields($fields);
-
-        $query = $model->query()
-            ->whereDate('date', '>=', $this->from->format('Y-m-d'))
-            ->whereDate('date', '<=', $this->to->format('Y-m-d'))
-            // ->where('Team', 'like', $this->team)
-            // ->where('Dial Group', 'like', $this->dial_group)
-            ->groupByRaw(join(', ', array_values($this->fields)))
-        ;
-
-        foreach ($this->fields as $value) {
-            $query->selectRaw("{$value}");
-            $query->orderByRaw($value);
+        foreach ($fields as $field => $value) {
+            $this->query->addSelect($field)
+                ->where($field, 'like', $value)
+                ->groupBy($field)
+                ->orderBy($field);
         }
 
-        return $query
-            ->selectRaw($this->aggregates());
-    }
-
-    protected function protectAgainstInvalidField(array $fields): self
-    {
-        foreach ($fields as $value) {
-            if (!in_array($value, array_keys($this->default_fields))) {
-                throw new Exception("Field {$value} is not included in default fields. Please remove or pass it with the ->addField('key', 'value') method!", 403);
-            }
-        }
-
-        return $this;
-    }
-
-    protected function updateFields(array $fields): self
-    {
-        $this->fields = empty($fields) ?
-            $this->default_fields :
-            array_intersect($this->default_fields, $fields);
-
-        return $this;
+        return $this->query;
     }
 }
---
+++ tests/Feature/Dashboards/RingCentral/RoutesTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Feature\Dashboards\RingCentral;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RoutesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function unauthenticated_users_can_not_see_dashboards()
+    {
+        $this->get(route('admin.dashboards.ring_central'))
+            ->assertRedirect(route('login'));
+    }
+
+    /** @test */
+    public function users_without_any_role_are_rendered_with_default_dashboard()
+    {
+        $this->actingAs($this->user());
+
+        $response = $this->get(route('admin.dashboards.ring_central'));
+
+        $response->assertForbidden();
+    }
+
+    /** @test */
+    public function users_with_role_ring_central_manager_are_rendered_with_dashboard()
+    {
+        $this->actingAs($this->userWithPermission('view-ring-central-dashboard'));
+
+        $response = $this->get(route('admin.dashboards.ring_central'));
+
+        $response->assertOk();
+        $response->assertViewIs('dashboards.ring-central.manager');
+    }
+}
