620f3a75f45af94d6340bac0c71e55d06b4c82b6
Yismen Jorge <yismen.jorge@gmail.com>
2023-03-08 18:44:59 -0400

    Fix bug with undefined variable

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.'.$layout->site(), ['page_header'=>'Home', 'page_description'=>'Home page!',
+@extends('layouts.site', ['page_header'=>'Home', 'page_description'=>'Home page!',
 'hide_content_header'=>false])
 
 @section('content')
