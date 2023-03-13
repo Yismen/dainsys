69d581637c755956ee5546fb7a77d35d440fffee
Yismen Jorge <yismen.jorge@gmail.com>
2023-03-13 13:00:28 -0400

    Update prunable models

---
+++ tests/Unit/NotificationTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Unit;
+
+use Tests\TestCase;
+use App\Models\Notification;
+use Illuminate\Database\Console\PruneCommand;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class NotificationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_removes_prunable_notifications()
+    {
+        $regular = factory(Notification::class)->create(['created_at' => now()]);
+        $prunable = factory(Notification::class)->create(['created_at' => now()->subDays(15)]);
+
+        $this->artisan(PruneCommand::class, [
+            '--model' => [
+                Notification::class
+            ],
+        ]);
+
+        $this->assertDatabaseHas('notifications', ['data' => $regular->data]);
+        $this->assertDatabaseMissing('notifications', ['data' => $prunable->data]);
+    }
+}
