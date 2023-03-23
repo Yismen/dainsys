7ea2bad83f4c471b0f90a586e05e7045185b1d50
Yismen Jorge <yismen.jorge@gmail.com>
2023-03-23 08:34:25 -0400

    Add Hotel Planning report

---
+++ app/Console/Commands/RingCentralReports/Exports/Sheets/HotelPlanning/ProductionSheet.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Console\Commands\RingCentralReports\Exports\Sheets\HotelPlanning;
+
+use Illuminate\View\View;
+use Illuminate\Support\Str;
+use Maatwebsite\Excel\Sheet;
+use App\Exports\RangeFormarter;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
+use Maatwebsite\Excel\Events\AfterSheet;
+use App\Console\Commands\RingCentralReports\Exports\Sheets\BaseRingCentralSheet;
+use App\Console\Commands\RingCentralReports\Exports\Support\Connections\ConnectionContract;
+use App\Console\Commands\RingCentralReports\Exports\Support\Connections\RingCentralConnection;
+
+class ProductionSheet extends BaseRingCentralSheet
+{
+    public function getData(ConnectionContract $connection, string $date_from, string $date_to): array
+    {
+        return $connection->connect()
+            ->select(
+                DB::raw("
+                    SELECT        
+                        [Dial Group] as dial_group 
+                        , CONVERT(date, [Login DTS]) AS production_date
+						, Team as team
+                        , trim([First Name] + ' ' + [Last Name]) AS agent_name
+                        , SUM(CONVERT(float, REPLACE([Login Time (min)], ',', ''))) / 60 AS login_time
+                    FROM   dbo.Agent_Session_Report_Raw
+                    WHERE
+                        CONVERT(date, [Login DTS]) BETWEEN '{$date_from}' AND '{$date_to}'
+                        AND [Dial Group] LIKE '{$this->exporter->campaign_name}'
+                        AND Team like '{$this->exporter->team}'
+                    GROUP BY 
+                        [Dial Group]
+                        , Team
+                        , [First Name]
+                        , [Last Name]
+						, CONVERT(date, [Login DTS])
+                    ORDER BY
+                        [Dial Group]
+						, CONVERT(date, [Login DTS])
+                        , Team
+                        , [First Name]
+						, [Last Name]
+                ")
+            );
+    }
+
+    /**
+     * @return View
+     */
+    public function view(): View
+    {
+        $class_name = Str::snake(class_basename($this));
+
+        $this->data = $this->getData(new RingCentralConnection(), $this->exporter->dates_range['from_date'], $this->exporter->dates_range['to_date']);
+
+        if (count($this->data) > 0) {
+            $this->exporter->has_data = true;
+
+            $cache_key = $this->exporter->campaign_name . $this->exporter->team . $this->exporter->dates_range['from_date'] . $this->exporter->dates_range['to_date'] . collect($this->data)->sum('login_time');
+
+            if (!Cache::has($cache_key)) {
+                Cache::forever($cache_key, 'any');
+
+                $this->exporter->data_is_new = true;
+            }
+        }
+
+        return view(
+            "exports.reports.ring_central.hotel_planning.{$class_name}",
+            [
+                'title' => "{$this->title()} From {$this->exporter->dates_range['from_date']} To {$this->exporter->dates_range['to_date']}",
+                'data' => $this->data,
+            ]
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function title(): string
+    {
+        return "{$this->exporter->client_name} Hours";
+    }
+
+    /**
+     * @return array
+     */
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                $rows = count($this->data) + 2;
+                $totals_row = $rows + 1;
+                $sheet = $event->sheet->getDelegate();
+                $last_column = 'E';
+
+                $this->addSubTotals($totals_row, $rows, $event->sheet);
+
+                $formarter = new RangeFormarter($event, "A1:{$last_column}{$rows}");
+
+                $formarter->configurePage()
+                    ->formatTitle('A1:D1')
+                    ->setAutoSizeRange('B', 'E')
+                    ->freezePane('A3')
+                    ->setAutoFilter("A2:{$last_column}{$rows}")
+                    ->formatHeaderRow("A2:{$last_column}2")
+                    ->setColumnsRangeWidth('A', 'A', 20)
+                    ->applyBorders("A3:{$last_column}{$rows}")
+                    ->applyNumberFormats("E3:E{$totals_row}", '_(* #,##0.00_);_(* (#,##0.00);_(* "-"??_);_(@_)')
+                    ->formatTotals("E{$totals_row}:E{$totals_row}");
+            },
+        ];
+    }
+
+    public function addSubTotals(int $totals_row, int $rows, Sheet $sheet_object)
+    {
+        $loginTimeColumn = 'E';
+
+        $sheet_object->setCellValue(
+            "{$loginTimeColumn}{$totals_row}",
+            "=SUBTOTAL(9, {$loginTimeColumn}3:{$loginTimeColumn}{$rows})"
+        );
+    }
+}
---
+++ resources/views/exports/reports/ring_central/hotel_planning/production_sheet.blade.php
@@ -0,0 +1,24 @@
+<h4>{{ $title }}</h4>
+<table>
+    <tbody>
+        <tr>
+            <th>Dial Group</th>
+            <th>Date</th>
+            <th>Team</th>
+            <th>Agent Name</th>
+            <th>Login Time</th>
+        </tr>
+
+        @foreach ($data as $row)
+        <tr>
+            <td>{{ $row->dial_group }}</td>
+            <td>{{ $row->production_date }}</td>
+            <td>{{ $row->team }}</td>
+            <td>{{ $row->agent_name }}</td>
+            <td>{{ $row->login_time }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+{{-- $repo->data['mtd']->sum('calls_offered') --}}
