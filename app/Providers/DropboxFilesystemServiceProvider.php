358f2d2bc567d668e3a1dd7490811804b1a45f59
Yismen Jorge <yismen.jorge@gmail.com>
2023-03-23 07:28:56 -0400

    Update to Larave 9

--- app/Providers/DropboxFilesystemServiceProvider.php
+++ app/Providers/DropboxFilesystemServiceProvider.php
@@ -2,20 +2,24 @@
 
 namespace App\Providers;
 
+use Spatie\Dropbox\Client ;
+use League\Flysystem\Filesystem;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
-use League\Flysystem\Filesystem;
-use Spatie\Dropbox\Client as DropboxClient;
 use Spatie\FlysystemDropbox\DropboxAdapter;
+use Illuminate\Filesystem\FilesystemAdapter;
 
 class DropboxFilesystemServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         Storage::extend('dropbox', function ($app, $config) {
-            $client = new DropboxClient($config['accessToken']);
+            $client = new Client($config['accessToken']);
 
-            return new Filesystem(new DropboxAdapter($client));
+            $adapter = new DropboxAdapter($client);
+            $driver = new Filesystem($adapter, ['case_sensitive' => false]);
+
+            return new FilesystemAdapter($driver, $adapter);
         });
     }
 
